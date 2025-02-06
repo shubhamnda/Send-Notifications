2d95ec350cbe0a77b1f0f38e0c01dc3add55c2e7
Shubham Nanda <124252565+shubhamnda@users.noreply.github.com>
2025-02-06 16:44:33 +0530

    notif 0.2

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { Client, Users, Messaging } from 'node-appwrite';
 export default async ({ req, res, log, error }) => {
   const client = new Client()
     .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
-    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID);
+    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID).setKey(process.env.APPWRITE_API_KEY);
    
   
   const users = new Users(client);
